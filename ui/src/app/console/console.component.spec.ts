8cfae3af31593ce25d38f06e84f3a67b525ef7bb
Karl Woditsch <k.woditsch@gmail.com>
2024-12-29 20:08:06 +0100

    Decode color codes and style codes

--- ui/src/app/console/console.component.spec.ts
+++ ui/src/app/console/console.component.spec.ts
@@ -3,6 +3,8 @@ import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BehaviorSubject } from 'rxjs';
 import { RconService } from 'src/services';
+import colorCodes from '../../config/color-codes.json';
+import styleCodes from '../../config/style-codes.json';
 import { ConsoleComponent } from './console.component';
 
 describe('ConsoleComponent', () => {
@@ -32,8 +34,8 @@ describe('ConsoleComponent', () => {
             .and
             .returnValue(new BehaviorSubject("test response"));
 
-        component.lastReply$.subscribe((value) => {
-            expect(value).toEqual("test response");
+        component.lastDecodedReply$.subscribe((value) => {
+            expect(value).toEqual(component.decodeResponse("test response"));
         });
 
         component.commandForm.setValue({ command: "test" });
@@ -42,13 +44,13 @@ describe('ConsoleComponent', () => {
         expect(spy).toHaveBeenCalledWith("test");
     });
 
-    it("should display the last reply", () => {
+    it("should display the last decoded reply", () => {
         fixture.detectChanges();
 
         let lastReplyCard = fixture.nativeElement.querySelector(".card");
         expect(lastReplyCard).toBeNull();
 
-        component.lastReply$.next("test response");
+        component.lastDecodedReply$.next(component.decodeResponse("test response"));
         fixture.detectChanges();
 
         lastReplyCard = fixture.nativeElement.querySelector(".card");
@@ -61,6 +63,22 @@ describe('ConsoleComponent', () => {
         component.onReset();
 
         expect(component.commandForm.value.command).toBeNull();
+    });
+
+    it("should decode the color codes", () => {
+        for (let colorCode of ["1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]) {
+            expect(component.decodeResponse(`§${colorCode}test§r`)).toEqual(`<span style="color: ${colorCodes[colorCode]};">test</span>`);
+        };
+    });
+
+    it("should decode the style codes", () => {
+        for (let styleCode of ["k", "l", "m", "n", "o"]) {
+            expect(component.decodeResponse(`§${styleCode}test§r`)).toEqual(`<span style="${styleCodes[styleCode]};">test</span>`);
+        };
+    });
+
+    it("should decode new lines", () => {
+        expect(component.decodeResponse("test\ntest")).toEqual("test<br>test");
     });
 
 });
--- ui/src/app/console/console.component.ts
+++ ui/src/app/console/console.component.ts
@@ -1,8 +1,11 @@
 import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Subject } from 'rxjs';
 import { RconService } from 'src/services';
+import colorCodes from '../../config/color-codes.json';
+import styleCodes from '../../config/style-codes.json';
 import { IconsModule, LocalizePipe } from '../core';
 
 @Component({
@@ -16,25 +19,59 @@ import { IconsModule, LocalizePipe } from '../core';
 })
 export class ConsoleComponent {
 
+    public readonly bypassSecurityTrustHtml: (value: string) => SafeHtml;
+
     public readonly commandForm: FormGroup<{
         command: FormControl<string | null>
     }> = new FormGroup({
         command: new FormControl(null),
     });
 
-    public readonly lastReply$: Subject<string> = new Subject();
+    public readonly lastDecodedReply$: Subject<string> = new Subject();
 
     constructor(
         private readonly rconService: RconService,
+        private readonly sanitizer: DomSanitizer,
     ) {
-
+        this.bypassSecurityTrustHtml = this.sanitizer.bypassSecurityTrustHtml.bind(this.sanitizer);
     }
 
+    /**
+     * Decodes the response from the RCON server
+     * 
+     * @param text The text to decode
+     * 
+     * @returns The decoded text
+     */
+    public decodeResponse(text: string): string {
+
+        // Replace new lines with <br> tags
+        text = text.replace(/\n/g, "<br>");
+
+        // Replace color codes with spans
+        text = text.replace(/§([0-9a-f])/g, (match, group) => {
+            return `<span style="color: ${colorCodes[group]};">`;
+        });
+
+        // Replace style codes with spans
+        text = text.replace(/§([k-o])/g, (match, group) => {
+            return `<span style="${styleCodes[group]};">`;
+        });
+
+        // Replace reset code with closing span
+        text = text.replace(/§r/g, "</span>");
+
+        return text;
+    }
+
+    /**
+     * Sends the command to the RCON server
+     */
     public onSubmit(): void {
         const command = this.commandForm.value.command;
 
         this.rconService.sendCommand(command).subscribe((reply: string) => {
-            this.lastReply$.next(reply);
+            this.lastDecodedReply$.next(this.decodeResponse(reply));
         });
     }
 
